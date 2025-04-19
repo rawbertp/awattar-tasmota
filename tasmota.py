30199022b95c7b03e021a7d7a34ba2be76c10802
Robert Porisenko <robert.porisenko@bearingpoint.com>
2025-04-19 23:00:05 +0200

    Add power control and improve logging

--- tasmota.py
+++ tasmota.py
@@ -1,3 +1,4 @@
+import logging
 from awattar import get_results
 import datetime
 import requests
@@ -5,6 +6,14 @@ import urllib.parse
 import json
 import os
 import sys
+
+# Configure the logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+logger = logging.getLogger(__name__)
 
 TASMOTA_WEB_USER = os.environ["TASMOTA_WEB_USER"]
 TASMOTA_WEB_PASSWORD = os.environ["TASMOTA_WEB_PASS"]
@@ -17,13 +26,14 @@ SLOT_HOURS = int(os.environ.get("SLOT_HOURS", "5"))
 
 ALWAYS_ENABLE = False
 
+logger.info("Fetching slot results...")
 slot = get_results([SLOT_HOURS])
 
 start_time = datetime.datetime.strptime(slot[0][1][0], "%Y-%m-%d %H:%M:%S")
 end_time = start_time + datetime.timedelta(hours=SLOT_HOURS)
 
 if start_time.hour >= end_time.hour:
-    print(f"It looks like start and end time are not on the same day - this is currently not supported yet!")
+    logger.error("Start and end time are not on the same day - this is currently not supported!")
     sys.exit(1)
 
 start_hrs_str = datetime.datetime.strftime(start_time, "%H:%M")
@@ -48,29 +58,50 @@ timer_1_data = {
     "Action": 1,
 } | timer_data
 
-
 timer_2_data = {
     "Time": end_hrs_str,
     "Action": 0,
 } | timer_data
 
-
 timer_1_url += urllib.parse.quote(json.dumps(timer_1_data))
 timer_2_url += urllib.parse.quote(json.dumps(timer_2_data))
 
 try:
-    print("Updating timer 1...", timer_1_data)
+    logger.info("Updating timer 1...")
+    logger.debug(f"Timer 1 data: {timer_1_data}")
     response = requests.get(timer_1_url)
     response.raise_for_status()
+    logger.info("Timer 1 update OK!")
 
-    print("Timer 1 update OK!")
-
-    print("Updating timer 2...", timer_2_data)
+    logger.info("Updating timer 2...")
+    logger.debug(f"Timer 2 data: {timer_2_data}")
     response = requests.get(timer_2_url)
     response.raise_for_status()
+    logger.info("Timer 2 update OK!")
 
-    print("Timer 2 update OK!")
+    # check if power should be on or off
+    # Get the current hour
+    current_hour = datetime.datetime.now().hour
+    if current_hour >= start_time.hour and current_hour < end_time.hour:
+        # If the current hour is within the range, turn on the power
+        logger.info("Power should be ON")
+        power_url = f"{TASMOTA_WEB_BASE_URL}&cmnd=Power1%20On"
+    else:
+        # If the current hour is outside the range, turn off the power
+        logger.info("Power should be OFF")
+        power_url = f"{TASMOTA_WEB_BASE_URL}&cmnd=Power1%20Off"
+    logger.info("Updating power state...")
+    logger.debug(f"Power URL: {power_url}")
+    response = requests.get(power_url)
+    response.raise_for_status()
+    logger.info("Power state update OK!")
+
+    logger.info("Getting power state...")
+    response = requests.get(f"{TASMOTA_WEB_BASE_URL}&cmnd=Power")
+    response.raise_for_status()
+    power_state = response.json().get("POWER")
+    logger.info(f"Power state: {power_state}")
 
 except Exception as e:
-    print("Timer updates FAILED!")
+    logger.error("Timer updates FAILED!", exc_info=True)
     sys.exit(1)
